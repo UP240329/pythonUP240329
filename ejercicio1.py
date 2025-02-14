899e171ba2dc304c1354f5881bcc0dcd72e8856d
UP240329 <up240329@alumnos.upa.edu.mx>
2025-02-13 19:02:45 -0600

    Avance de ejercicios día 3

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,24 @@
 age=18
+print(type(age))
+height=75.5
+print(type(height))
+num=2j
+print(type(num))
+
+base=float(input("Ingresa la medida de la base: "))
+altura=float(input("Ingresa la altura del triángulo: "))
+area=(base*altura)/2
+print("El área del triángulo es: ",area)
+
+ladoA=float(input("Enter side a: "))
+ladoB=float(input("Enter side b: "))
+ladoC=float(input("Enter side c: "))
+perimeter=ladoA+ladoB+ladoC
+print("The perimeter of the triangle is: ",perimeter)
+
+length=float(input("Enter length of the rectangle: "))
+width=float(input("Enter width of the rectangle: "))
+area=length*width
+perimeter=2*(length+width)
+print("The perimeter of the triangle is: ")
+print("The perimeter and area of the triangle is: ",perimeter," , "+area)
