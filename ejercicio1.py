f882f7442d9cf0b9763d9f3657b698dbcf17a3b2
UP240329 <up240329@alumnos.upa.edu.mx>
2025-02-19 15:02:08 -0600

    Fin del día 3

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,4 @@
+
 age=18
 print(type(age))
 height=75.5
@@ -45,9 +46,67 @@ distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 print("Slope: ",slope,"Distance: ", distance)
 #10
 print("Slopes are equal" if pendiente == slope else "Slopes are different")
-#11
+#11 Calculate the value of y (y = x^2 + 6x + 9). Try to use different 
+# x values and figure out at what x value y is going to be 0.
+x = int(input("Inserta el valor de x:"))
+y = x**2 + 6*x + 9
+print("El valor de y es: ", y)
+#11.1
+x = int(input("Inserta el valor de x: "))
+y = x**2 + 6*x + 9
+print("El valor de y es: ", y)
+if y == 0:
+    print("y es 0 cuando x es", x)
+else:
+    print("y no es 0 cuando x es", x)
+#11.2
+x = -3
+y = x**2 + 6*x + 9
+print("El valor de y es: ", y)  # This should print "El valor de y es:  0"
+#12 Find the length of 'python' and 'dragon' and make a falsy comparison statement.
+print(len('python')<len('dragon'))
+#13 Use and operator to check if 'on' is found in both 'python' and 'dragon'
+print('on' in 'python' and 'on' in 'dragon')
+#14 I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+sentence = 'I hope this course is not full of jargon'
+print('jargon' in sentence)
+#15 There is no 'on' in both dragon and python
+print('on' not in 'python' and 'on' not in 'dragon')
+#16 Find the length of the text python and convert the value to float and convert it to string
+print(str(float(len('python'))))
+#17 Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+num= int(input("Enter a number: "))
+if num % 2 == 0:
+    print(num," is an even number.")
+else:
+    print(num," is an odd number.")
+#18 The floor division of 7 by 3 is equal to the int converted value of 2.7. make this statement true.
+print(7//3 == int(2.7))
+#19 Check if type of '10' is equal to 10
+print(type('10')==type(10))
+#20 Check if int('9.8') is equal to 10
+print(int(float('9.8'))==10)
+#21 Writ a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+hours = int(input("Enter hours: "))     
+rate = int(input("Enter rate per hour: "))
+pay = hours * rate
+print("Your weekly earning is: ", pay)
+#22 Write a script that prompts the user to enter number of years. Calculate the number of seconds a person can live. Assume a person can live hundred years
+years = int(input("Enter number of years you have lived: "))
+seconds = years * 365 * 24 * 60 * 60    
+print("You have lived for ", seconds, " seconds.")
+#23 Write a python script that displays the following table
+# 1 1 1 1 1
+# 2 1 2 4 8
+# 3 1 3 9 27
+# 4 1 4 16 64
+# 5 1 5 25 125
+print("1 1 1 1 1")
+print("2 1 2 4 8")
+print("3 1 3 9 27")
+print("4 1 4 16 64")
+print("5 1 5 25 125")
 
-#12
-print(len('python')>len('dragon'))
-#13
+
+
 
