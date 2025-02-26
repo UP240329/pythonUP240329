3dc9fc38f0cb6825d35195dbadc066e88c34c149
UP240329 <up240329@alumnos.upa.edu.mx>
2025-02-26 10:37:04 -0600

    Avance

--- dia4.py
+++ dia4.py
@@ -66,10 +66,7 @@ example="You cannot end a sentence with because because because is a conjunction
 example=example.split()
 print(example.index("because"))
 #Use rindex to find the position of the last occurrence of the word because in the following sentence: 
-# 'You cannot end a sentence with because because because is a conjunction'
-example="You cannot end a sentence with because because because is a conjunction"
-example=example.split()
-
+print()
 
 
 
