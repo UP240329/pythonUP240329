fba7bc5aae29df98159eb30a240f2d33b292c843
UP240329 <up240329@alumnos.upa.edu.mx>
2025-03-12 10:33:56 -0600

    Colab con Javi

---
+++ dia8.py
@@ -0,0 +1,13 @@
+#Create an empty dictionary called dog
+dog = {}
+#Add name, color, breed, legs, age to the dog dictionary
+dog['key1']='Javier'
+print(dog)
+dog['key2']='Negro'
+print(dog)
+dog['key3']='Pastor Alemán'
+print(dog)
+dog['key4']='4'
+print(dog)
+dog['key5']='2'
+print(dog)
