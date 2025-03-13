06deadfa2953a2d6ac47a8dc5b52eb9abe4b7782
UP240329 <up240329@alumnos.upa.edu.mx>
2025-03-13 11:45:08 -0600

    Avance

--- 08_Day/dia8.py
+++ 08_Day/dia8.py
@@ -2,12 +2,28 @@
 dog = {}
 #Add name, color, breed, legs, age to the dog dictionary
 dog['key1']='Javier'
-print(dog)
 dog['key2']='Negro'
-print(dog)
 dog['key3']='Pastor Alemán'
-print(dog)
 dog['key4']='4'
-print(dog)
 dog['key5']='2'
 print(dog)
+#Create a student dictionary and add first_name, last_name
+#  gender, age, marital status, skills, country, city and address as keys for the dictionary
+students={
+    'Name':'Monica',
+    'LastName':'Escobedo',
+    'Gender':'Female',
+    'Age':'18',
+    'MaritalStatus':'Soltera',
+    'Skills':['Weight lifting', 'hacer enojar a Javier'],
+    'Country':'México',
+    'City':'Aguascalientes',
+    'Address':{
+        'Street':'Priv. Ave del Paraíso #226',
+        'Localidad':'El llano Jesús María, Aguascalientes'
+        }
+}
+#Get the length of the student dictionary
+print(len(students))
+#Get the value of skills and check the data type, it should be a list
+print(students['Skills'])
