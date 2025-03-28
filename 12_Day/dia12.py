98334a43d92f6732e60bb9f30a34478d1a2e3f19
UP240329 <up240329@alumnos.upa.edu.mx>
2025-03-27 21:18:56 -0600

    A mimir

--- 12_Day/dia12.py
+++ 12_Day/dia12.py
@@ -42,3 +42,31 @@ def list_of_hexa_colors():
         hex_colors.append(hex_color)
     return hex_colors
 print(list_of_hexa_colors())
+# Escriba una función llamada list_of_rgb_colors que devuelva cualquier número de colores RGB en una matriz (tres valores de 0 a 255 cada uno).
+def list_of_rgb_colors():
+    rgb_colors = []
+    for i in range(10):
+        rgb_color = rgb_color_gen()
+        rgb_colors.append(rgb_color)
+    return rgb_colors
+print(list_of_rgb_colors())
+# Escriba una función llamada generate_colors que genere colores hexadecimales y RGB. La función debe aceptar un número entero como parámetro.
+def generate_colors(n):
+    hex_colors = list_of_hexa_colors()  
+    rgb_colors = list_of_rgb_colors()
+    colors = []
+    for i in range(n):
+        colors.append((hex_colors[i], rgb_colors[i]))
+    return colors   
+print(generate_colors(5))
+#Nivel3
+#Llama a tu función shuffle_list, toma una lista como parámetro y devuelve una lista aleatoria
+# (la lista se puede mezclar con la función shuffle de random).
+def shuffle_list(lst):
+    shuffle(lst)
+    return lst
+print(shuffle_list([1, 2, 3, 4, 5]))
+#Escriba una función que devuelva un array de siete números aleatorios en un rango del 0 al 9. Todos los números deben ser únicos
+def unique_random_numbers():
+    return sample(range(10), 7)
+print(unique_random_numbers())
