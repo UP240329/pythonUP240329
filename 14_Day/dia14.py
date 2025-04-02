2e00c42f67f6f51d10fc5092c9da2647a917c2a5
UP240329 <up240329@alumnos.upa.edu.mx>
2025-04-01 20:14:45 -0600

    ya

---
+++ 14_Day/dia14.py
@@ -0,0 +1,20 @@
+#Explique la diferencia entre mapa, filtro y reducción.
+#map aplica una función a cada elemento de una lista y devuelve una nueva lista con los resultados.
+#filter aplica una función a cada elemento de una lista y devuelve una nueva lista con los elementos que cumplen la condición.
+#reduce aplica una función a los elementos de una lista y devuelve un solo valor acumulado.
+#########
+#Explique la diferencia entre función de orden superior, cierre y decorador.
+#Una función de orden superior es una función que toma otra función como argumento o devuelve una función como resultado.
+#Un cierre es una función que recuerda el entorno en el que fue creada, incluso si se ejecuta fuera de ese entorno. 
+#Un decorador es una función que toma otra función y extiende su comportamiento sin modificarla directamente.
+#########
+#Defina una función de llamada antes de map, filter o reduce, vea los ejemplos.
+
+
+
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#Utilice el bucle for para imprimir cada país en la lista de países.
+#Utilice for para imprimir cada nombre en la lista de nombres.
+#Utilice for para imprimir cada número en la lista de números.
