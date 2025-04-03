7c899aef90c3aa2c1b43206a4ffa48fdfd5f04c2
UP240329 <up240329@alumnos.upa.edu.mx>
2025-04-02 18:45:59 -0600

    El día más fácil de todos

---
+++ 15_Day/dia15.py
@@ -0,0 +1,24 @@
+print('Errores en Python, todos los programas tienen algún error')
+
+#Syntaxerror
+print('Pongame 10 profe'
+#NameError
+print(monica)
+#IndexError
+edades=[1,2,3,4,5]
+print(edades[10])
+#ModuleNotFoundError
+import UPA
+#AttributeError
+import range
+range.ends
+#KeyError
+estudiante={'nombre':'monica','edad':20}
+print(estudiante['nombe'])
+#TypeError
+print('Hola' + 10)
+#ValueError
+print(int('Hola'))
+#ImportError
+import math
+import math1
