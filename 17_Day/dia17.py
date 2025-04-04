82f6a465fd16d9febb07b0f63877a0727e5461fb
UP240329 <up240329@alumnos.upa.edu.mx>
2025-04-03 18:20:22 -0600

    .

---
+++ 17_Day/dia17.py
@@ -0,0 +1,6 @@
+#names = ['Finlandia', 'Suecia', 'Noruega', 'Dinamarca', 'Islandia', 'Estonia', 'Rusia']. Descomprima los primeros cinco países y almacénelos en una variable nordic_countries, almacene Estonia y Rusia en es y ru respectivamente.
+nordic_countries = ['Finlandia', 'Suecia', 'Noruega', 'Dinamarca', 'Islandia']
+es, ru = ['Estonia', 'Rusia']
+print("Nórdicos:", nordic_countries)
+print("Estonia:", es)
+print("Rusia:", ru)
